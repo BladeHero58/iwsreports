1065eb4762f270be7775f2113c1172cdc63bc5db
Tomi <bladehero01@gmail.com>
2025-05-11 17:13:56 +0200

    Budaőrs után 4

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,35 @@
 module.exports = {
-    development: {
-      client: 'pg',
-      connection: {
-        host: process.env.PGHOST,
-        port: process.env.PGPORT,
-        user: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE,
-      },
-      migrations: {
-        directory: './migrations',
-      },
-      seeds: {
-        directory: './seeds',
-      },
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
     },
-  };
+    migrations: {
+      directory: './migrations',
+    },
+    seeds: {
+      directory: './seeds',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+    },
+    migrations: {
+      directory: './migrations',
+    },
+    seeds: {
+      directory: './seeds',
+    },
+  },
+};
