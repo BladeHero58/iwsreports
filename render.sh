6129cfadbe7004db385d44b94f68c555e6c8e9a6
Tomi <bladehero01@gmail.com>
2025-05-14 14:04:46 +0200

    Render javaslat

--- render.sh
+++ render.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get install -y chromium
+apt-get install -y chromium-browser  # A pontos csomagnév chromium-browser
+# Ellenőrizzük, hogy telepítve van-e és hol található
+which chromium-browser
