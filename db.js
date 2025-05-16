466d613948e6437565cd91e47ac9c870374ee1bd
Tomi <bladehero01@gmail.com>
2025-05-16 08:13:41 +0200

    Adatbázis beállítás

--- db.js
+++ db.js
@@ -17,11 +17,7 @@ module.exports = pool;
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
--- reports.js
+++ reports.js
@@ -25,15 +25,14 @@ const pool = new Pool({
 */
 
 
+const { Pool } = require('pg');
+
 const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
-  });
-  
-  module.exports = pool;
+  connectionString: process.env.DATABASE_URL,
+});
+
+module.exports = pool;
+
 
 const router = express.Router(); 
 
