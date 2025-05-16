87e920b72c614a7d744fb6b63246c1c076a091c0
Tomi <bladehero01@gmail.com>
2025-05-16 09:45:52 +0200

    Adatbázis beállítás

--- migrate.js
+++ migrate.js
@@ -1,21 +1,24 @@
-const knex = require('knex')(require('./knexfile.js').development); // development környezet
+// Határozd meg a környezetet a NODE_ENV alapján (amit a Railway beállít)
+const environment = process.env.NODE_ENV || 'development';
+const knexConfig = require('./knexfile.js')[environment];
+const knex = require('knex')(knexConfig);
 
 async function runMigrations() {
   try {
-    console.log('Adatbázis kapcsolat ellenőrzése...');
-    await knex.raw('SELECT 1'); // Egyszerű lekérdezés az adatbázis kapcsolat ellenőrzésére
+    console.log(`Adatbázis kapcsolat ellenőrzése (${environment} környezet)...`);
+    await knex.raw('SELECT 1');
     console.log('Adatbázis kapcsolat sikeres!');
 
     console.log('Migrációk futtatása...');
     await knex.migrate.latest();
     console.log('Migrációk sikeresen futottak!');
 
-    process.exit(0); // Sikeres futás esetén kilépés
+    process.exit(0);
   } catch (error) {
     console.error('Hiba a migrációk futtatása közben:', error);
-    process.exit(1); // Hibás futás esetén kilépés
+    process.exit(1);
   } finally {
-    await knex.destroy(); // Adatbázis kapcsolat lezárása
+    await knex.destroy();
   }
 }
 
