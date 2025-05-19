6cfb04c24e2163d33b00606d9d3c3bcfbd8cc6d9
Tomi <bladehero01@gmail.com>
2025-05-19 09:19:46 +0200

    adatbázis 5

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
- /*
- const { Pool } = require('pg');
+/*
+const { Pool } = require('pg');
 
 
 const pool = new Pool({
     user: 'postgres', // PostgreSQL felhasználónév
-    host: 'localhost',     // Ha helyi gépen fut, ez marad
+    host: 'localhost',       // Ha helyi gépen fut, ez marad
     database: 'project_management', // Az általad létrehozott adatbázis neve
     password: 'dbzzed58', // Az adatbázishoz tartozó jelszó
-    port: 5432,            // PostgreSQL alapértelmezett portja
+    port: 5432,                 // PostgreSQL alapértelmezett portja
 });
 
 module.exports = pool;
@@ -18,6 +18,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
