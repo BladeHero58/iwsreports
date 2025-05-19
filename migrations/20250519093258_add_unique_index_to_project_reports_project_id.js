b6f0da09d3f6a3a656777bb0c350bcced3777bb0
Tomi <bladehero01@gmail.com>
2025-05-19 11:33:43 +0200

    Mentési és betöltési rendszer teljes átdolgozása 3

---
+++ migrations/20250519093258_add_unique_index_to_project_reports_project_id.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('project_reports', (table) => {
+    table.unique('project_id');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+   return knex.schema.alterTable('project_reports', (table) => {
+    table.dropUnique('project_id');
+  });
+};
