af38bdfc54b7a66ee18e0e4c9c54dc0e43344010
Tomi <bladehero01@gmail.com>
2025-05-28 14:37:11 +0200

    with help puppeter install manually

--- reports.js
+++ reports.js
@@ -698,10 +698,11 @@ router.get('/:projectId/download-pdf', async (req, res) => {
 `;
 
 // PDF generálás Puppeteerrel
+
 const browser = await puppeteer.launch({
-  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser',
+  executablePath: path.join(__dirname, '.cache', 'puppeteer', 'chrome'),
   headless: 'new',
-  args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+  args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
 });
 
 const page = await browser.newPage();
